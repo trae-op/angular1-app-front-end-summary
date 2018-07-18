a54b1af7ddbee01c03a0e7a5e97369084632c134
Pronchenko Oleg <traeop@gmail.com>
2018-07-18 22:24:34 +0300

    change branch and new build

--- code/dev/config.js
+++ code/dev/config.js
@@ -1,2 +1,6 @@
 
-window.API_URL = 'http://localhost:8000/api';
+// local api
+//window.API_URL = 'http://localhost:8000/api';
+
+// production api
+window.API_URL = 'https://app-front-end-summary.herokuapp.com/api';
